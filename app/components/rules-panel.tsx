2316f8d6583358a82ed397e435f03eacc29deae8
nmukassa1 <nmukassa1@gmail.com>
2025-08-18 07:17:21 +0100

    .

--- app/components/rules-panel.tsx
+++ app/components/rules-panel.tsx
@@ -90,7 +90,7 @@ export default function RulesPanel() {
 
       <Card className="bg-[#191414] border-[#282828]">
         <CardHeader className="pb-4 px-4 md:px-6">
-          <CardTitle className="text-white text-lg">Today's Stats</CardTitle>
+          <CardTitle className="text-white text-lg">Today&apos;s Stats</CardTitle>
         </CardHeader>
         <CardContent className="space-y-4 px-4 md:px-6">
           <div className="grid grid-cols-2 gap-4">
