2f63ee1f56cf6977de8c8f6056de2d5cd8b2aaf1
nmukassa1 <nmukassa1@gmail.com>
2025-08-18 19:37:34 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey="pk_test_Y29tcGxldGUtYm9hci00NC5jbGVyay5hY2NvdW50cy5kZXYk">
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-spotify-black`}
