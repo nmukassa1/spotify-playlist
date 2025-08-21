b9444aa8b934b464f5f5c163086a5bc86e1287aa
nmukassa1 <nmukassa1@gmail.com>
2025-08-22 00:17:14 +0100

    Next step

--- app/api/songs/route.ts
+++ app/api/songs/route.ts
@@ -14,6 +14,10 @@ export async function POST(request: NextRequest) {
 
     const songs = await fetchSongs(playlists);
     
+    /*
+      Analyse songs & segment
+    */
+    
     return NextResponse.json({
       success: true,
       songs,
